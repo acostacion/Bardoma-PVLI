539b01766a8a691195d11fa6f2ac8193892d3520
acostacion <carmengomezb27@gmail.com>
2024-12-16 17:35:11 +0100

    eliminatio

--- Introito Antiapotropaico/src/objetos/Game2Obj/Crocodile.js
+++
@@ -1,6 +0,0 @@
-import Obstacle from '../Game2Obj/Obstacle.js';
-export default class Crocodile extends Obstacle{
-    constructor(scene){ 
-        super(scene, 'crocodile');
-    }
-}
--- Introito Antiapotropaico/src/objetos/Game2Obj/Hippo.js
+++
@@ -1,6 +0,0 @@
-import Obstacle from '../Game2Obj/Obstacle.js';
-export default class Hippo extends Obstacle{
-    constructor(scene){ // Ponerle tambien obsGen aqui.
-        super(scene, 'hippo');
-    }
-}
